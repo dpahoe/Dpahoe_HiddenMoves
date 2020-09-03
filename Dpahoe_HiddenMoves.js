9421f80e82f309f494916d33b3e08578db317f08
Shahabaz Khan <shahabaz@loy.info>
2020-09-03 17:16:00 +0530

    v1.1

--- Dpahoe_HiddenMoves.js
+++ Dpahoe_HiddenMoves.js
@@ -2,7 +2,7 @@
 // Dpahoe_HiddenMoves.js
 //=============================================================================
 /*:
- * @plugindesc [1.0] Enables Hidden Moves (HM) usages outside battle 
+ * @plugindesc [1.1] Enables Hidden Moves (HM) usages outside battle 
  *
  * @author Dpahoe
  *
@@ -107,10 +107,19 @@
  * - You can enter upto 8 skills to be used as HM.
  * - Enter the skill's ID and the text to be shown while activating the HM.
  * - Enter Plugin Command: HMObject <skill ID> on any event that needs to be affected by that skill.
- * - Call a Common Event in the required skill's trait.
- * - In that Common Event, enter Plugin Command HMEvent <skill ID>
  * - Whenever that skill is used outside battle, the event with HMObject Plugin Command will turn it's Self Switch C ON,
  * if the event is triggered.
+ *
+ * Change Log 
+ * ---------- 
+ * v1.1 
+ * - Removed usage of external common events. Therefore HMEvent plugin command not required.
+ * - Added skill usage directly from interacting the HMObject.
+ * - Fixed a bug, where you use a skill and open menu again, the skill used state becomes lost.
+ *
+ * v1.0 
+ * - First version 
+
  */
 
 (function(){
@@ -147,14 +156,8 @@
 	object_texts[skill_g_id] = parameters['Skill-G Object text'] || 'Use %1 on this object';
 	object_texts[skill_h_id] = parameters['Skill-H Object text'] || 'Use %1 on this object';
 
-	var alias_Scene_Map_onMapLoaded = Scene_Map.prototype.onMapLoaded;
-	Scene_Map.prototype.onMapLoaded = function() {
-
-	    // Established map load routines.
-	    alias_Scene_Map_onMapLoaded.call(this);
-
-	    // Plugin addendum
-		hm_status_array[skill_a_id] = false;
+    function resetHmStatusArray(){
+    	hm_status_array[skill_a_id] = false;
 		hm_status_array[skill_b_id] = false;
 		hm_status_array[skill_c_id] = false;
 		hm_status_array[skill_d_id] = false;
@@ -162,7 +165,9 @@
 		hm_status_array[skill_f_id] = false;
 		hm_status_array[skill_g_id] = false;
 		hm_status_array[skill_h_id] = false;
-	}
+    }
+
+	resetHmStatusArray();
 
 	var _Game_Interpreter_pluginCommand = Game_Interpreter.prototype.pluginCommand;
     Game_Interpreter.prototype.pluginCommand = function(command, args) {
@@ -170,30 +175,53 @@
         // insert additional processing details here
 
         switch (command.toLowerCase()) {
-    		case "hmevent":
-    			let hmevent_skill_id = (args[0] || "").toLowerCase();
-    			if(!$gameParty.inBattle()){
-    				hm_status_array[hmevent_skill_id] = true;
-    				$gameMessage.add(skill_texts[hmevent_skill_id].replace('%1', $dataSkills[parseInt(hmevent_skill_id)].name));
-    			}
-    		break;
     		case "hmobject":
     			let hmobject_skill_id = (args[0] || "").toLowerCase();
+	    		let event_id = this._eventId;
     			if(hm_status_array[hmobject_skill_id]){
 	    			let map_id = $gameMap._mapId;
-	    			let event_id = this._eventId;
 					let key = [map_id, event_id, 'C'];
 					$gameSelfSwitches.setValue(key, true);
     			}
     			else{
     				$gameMessage.add(object_texts[hmobject_skill_id].replace('%1', $dataSkills[parseInt(hmobject_skill_id)].name));
+    				if($gameParty.members().some(function(actor) {return actor.hasSkill(hmobject_skill_id)})){
+    					$gameMessage.add('Use '+$dataSkills[parseInt(hmobject_skill_id)].name+'?');
+    					$gameMap._interpreter.setupChoices([['Yes', 'No'], 1]);
+    				}
+    				$gameMessage.setChoiceCallback(function(responseIndex) {
+					  if (responseIndex === 0) {
+					    // Player chose "Yes"
+					    let map_id = $gameMap._mapId;
+						let key = [map_id, event_id, 'C'];
+						$gameSelfSwitches.setValue(key, true);
+						resetHmStatusArray();
+						hm_status_array[hmobject_skill_id] = true;
+					  }
+					  else {
+					    // Player chose "No"
+					  }
+					});
     			}
     		break;
 
     		default:
     		break;
     	}
-
-
     };
+
+    var _Game_BattlerBase_paySkillCost = Game_BattlerBase.prototype.paySkillCost;
+    Game_BattlerBase.prototype.paySkillCost = function(skill) {
+    	_Game_BattlerBase_paySkillCost.call(this, skill);
+    	// insert additional processing details here
+
+		if(!$gameParty.inBattle()){
+			if(typeof hm_status_array[skill.id] !== 'undefined'){
+				resetHmStatusArray();
+				hm_status_array[skill.id] = true;
+	    		$gameMessage.add(skill_texts[skill.id].replace('%1', $dataSkills[parseInt(skill.id)].name));
+	    	}
+		}
+    }
+
 })();
