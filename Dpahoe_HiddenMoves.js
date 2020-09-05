9fa86ea81a9f4716d488048fb2c01557aedc9238
dpahoe <shahabaz@loy.info>
2020-09-05 20:30:56 +0530

    v1.2

--- Dpahoe_HiddenMoves.js
+++ Dpahoe_HiddenMoves.js
@@ -18,6 +18,10 @@
  * @desc Text to be displayed, when player interacts with an HM applicable object. %1 will be replaced by Skill name.
  * @default Use %1 on this object
  *
+ * @param Skill-A Show Skill Prompt
+ * @desc [ON/OFF] Choose whether to show a choice to use skill, when interating with HM object. Else player will have to use skill beforehand.
+ * @default ON
+ *
  * @param Skill-B ID
  * @desc Insert Skill ID found in the database
  * @default 0
@@ -29,6 +33,10 @@
  * @param Skill-B Object text
  * @desc Text to be displayed, when player interacts with an HM applicable object. %1 will be replaced by Skill name.
  * @default Use %1 on this object
+ *
+ * @param Skill-B Show Skill Prompt
+ * @desc [ON/OFF] Choose whether to show a choice to use skill, when interating with HM object. Else player will have to use skill beforehand.
+ * @default ON
  *
  * @param Skill-C ID
  * @desc Insert Skill ID found in the database
@@ -42,6 +50,10 @@
  * @desc Text to be displayed, when player interacts with an HM applicable object. %1 will be replaced by Skill name.
  * @default Use %1 on this object
  *
+ * @param Skill-C Show Skill Prompt
+ * @desc [ON/OFF] Choose whether to show a choice to use skill, when interating with HM object. Else player will have to use skill beforehand.
+ * @default ON
+ *
  * @param Skill-D ID
  * @desc Insert Skill ID found in the database
  * @default 0
@@ -53,6 +65,10 @@
  * @param Skill-D Object text
  * @desc Text to be displayed, when player interacts with an HM applicable object. %1 will be replaced by Skill name.
  * @default Use %1 on this object
+ *
+ * @param Skill-D Show Skill Prompt
+ * @desc [ON/OFF] Choose whether to show a choice to use skill, when interating with HM object. Else player will have to use skill beforehand.
+ * @default ON
  *
  * @param Skill-E ID
  * @desc Insert Skill ID found in the database
@@ -66,6 +82,10 @@
  * @desc Text to be displayed, when player interacts with an HM applicable object. %1 will be replaced by Skill name.
  * @default Use %1 on this object
  *
+ * @param Skill-E Show Skill Prompt
+ * @desc [ON/OFF] Choose whether to show a choice to use skill, when interating with HM object. Else player will have to use skill beforehand.
+ * @default ON
+ *
  * @param Skill-F ID
  * @desc Insert Skill ID found in the database
  * @default 0
@@ -77,6 +97,10 @@
  * @param Skill-F Object text
  * @desc Text to be displayed, when player interacts with an HM applicable object. %1 will be replaced by Skill name.
  * @default Use %1 on this object
+ *
+ * @param Skill-F Show Skill Prompt
+ * @desc [ON/OFF] Choose whether to show a choice to use skill, when interating with HM object. Else player will have to use skill beforehand.
+ * @default ON
  *
  * @param Skill-G ID
  * @desc Insert Skill ID found in the database
@@ -90,6 +114,10 @@
  * @desc Text to be displayed, when player interacts with an HM applicable object. %1 will be replaced by Skill name.
  * @default Use %1 on this object
  *
+ * @param Skill-G Show Skill Prompt
+ * @desc [ON/OFF] Choose whether to show a choice to use skill, when interating with HM object. Else player will have to use skill beforehand.
+ * @default ON
+ *
  * @param Skill-H ID
  * @desc Insert Skill ID found in the database
  * @default 0
@@ -102,6 +130,10 @@
  * @desc Text to be displayed, when player interacts with an HM applicable object. %1 will be replaced by Skill name.
  * @default Use %1 on this object
  *
+ * @param Skill-H Show Skill Prompt
+ * @desc [ON/OFF] Choose whether to show a choice to use skill, when interating with HM object. Else player will have to use skill beforehand.
+ * @default ON
+ *
  * @help
  *
  * - You can enter upto 8 skills to be used as HM.
@@ -112,6 +144,10 @@
  *
  * Change Log 
  * ---------- 
+ * v1.2
+ * - Fixed a bug where blank object and event texts are still showing
+ * - Added Show Skill Prompt parameter. Now you can choose whether to show the text prompt to use skill when interacting with the object
+ *
  * v1.1 
  * - Removed usage of external common events. Therefore HMEvent plugin command not required.
  * - Added skill usage directly from interacting the HMObject.
@@ -137,24 +173,34 @@
 	let hm_status_array = {};
 	let skill_texts = {};
 	let object_texts = {};
+	let hm_prompts = {};
 
-	skill_texts[skill_a_id] = parameters['Skill-A Event text'] || '%1 is usable now!';
-	skill_texts[skill_b_id] = parameters['Skill-B Event text'] || '%1 is usable now!';
-	skill_texts[skill_c_id] = parameters['Skill-C Event text'] || '%1 is usable now!';
-	skill_texts[skill_d_id] = parameters['Skill-D Event text'] || '%1 is usable now!';
-	skill_texts[skill_e_id] = parameters['Skill-E Event text'] || '%1 is usable now!';
-	skill_texts[skill_f_id] = parameters['Skill-F Event text'] || '%1 is usable now!';
-	skill_texts[skill_g_id] = parameters['Skill-G Event text'] || '%1 is usable now!';
-	skill_texts[skill_h_id] = parameters['Skill-H Event text'] || '%1 is usable now!';
+	hm_prompts[skill_a_id] = parameters['Skill-A Show Skill Prompt'].toLowerCase() == 'on';
+	hm_prompts[skill_b_id] = parameters['Skill-B Show Skill Prompt'].toLowerCase() == 'on';
+	hm_prompts[skill_c_id] = parameters['Skill-C Show Skill Prompt'].toLowerCase() == 'on';
+	hm_prompts[skill_d_id] = parameters['Skill-D Show Skill Prompt'].toLowerCase() == 'on';
+	hm_prompts[skill_e_id] = parameters['Skill-E Show Skill Prompt'].toLowerCase() == 'on';
+	hm_prompts[skill_f_id] = parameters['Skill-F Show Skill Prompt'].toLowerCase() == 'on';
+	hm_prompts[skill_g_id] = parameters['Skill-G Show Skill Prompt'].toLowerCase() == 'on';
+	hm_prompts[skill_h_id] = parameters['Skill-H Show Skill Prompt'].toLowerCase() == 'on';
 
-	object_texts[skill_a_id] = parameters['Skill-A Object text'] || 'Use %1 on this object';
-	object_texts[skill_b_id] = parameters['Skill-B Object text'] || 'Use %1 on this object';
-	object_texts[skill_c_id] = parameters['Skill-C Object text'] || 'Use %1 on this object';
-	object_texts[skill_d_id] = parameters['Skill-D Object text'] || 'Use %1 on this object';
-	object_texts[skill_e_id] = parameters['Skill-E Object text'] || 'Use %1 on this object';
-	object_texts[skill_f_id] = parameters['Skill-F Object text'] || 'Use %1 on this object';
-	object_texts[skill_g_id] = parameters['Skill-G Object text'] || 'Use %1 on this object';
-	object_texts[skill_h_id] = parameters['Skill-H Object text'] || 'Use %1 on this object';
+	skill_texts[skill_a_id] = parameters['Skill-A Event text'] || '';
+	skill_texts[skill_b_id] = parameters['Skill-B Event text'] || '';
+	skill_texts[skill_c_id] = parameters['Skill-C Event text'] || '';
+	skill_texts[skill_d_id] = parameters['Skill-D Event text'] || '';
+	skill_texts[skill_e_id] = parameters['Skill-E Event text'] || '';
+	skill_texts[skill_f_id] = parameters['Skill-F Event text'] || '';
+	skill_texts[skill_g_id] = parameters['Skill-G Event text'] || '';
+	skill_texts[skill_h_id] = parameters['Skill-H Event text'] || '';
+
+	object_texts[skill_a_id] = parameters['Skill-A Object text'] || '';
+	object_texts[skill_b_id] = parameters['Skill-B Object text'] || '';
+	object_texts[skill_c_id] = parameters['Skill-C Object text'] || '';
+	object_texts[skill_d_id] = parameters['Skill-D Object text'] || '';
+	object_texts[skill_e_id] = parameters['Skill-E Object text'] || '';
+	object_texts[skill_f_id] = parameters['Skill-F Object text'] || '';
+	object_texts[skill_g_id] = parameters['Skill-G Object text'] || '';
+	object_texts[skill_h_id] = parameters['Skill-H Object text'] || '';
 
     function resetHmStatusArray(){
     	hm_status_array[skill_a_id] = false;
@@ -184,24 +230,33 @@
 					$gameSelfSwitches.setValue(key, true);
     			}
     			else{
-    				$gameMessage.add(object_texts[hmobject_skill_id].replace('%1', $dataSkills[parseInt(hmobject_skill_id)].name));
-    				if($gameParty.members().some(function(actor) {return actor.hasSkill(hmobject_skill_id)})){
-    					$gameMessage.add('Use '+$dataSkills[parseInt(hmobject_skill_id)].name+'?');
-    					$gameMap._interpreter.setupChoices([['Yes', 'No'], 1]);
+    				if(typeof object_texts[hmobject_skill_id] !== 'undefined'){
+	    				if(object_texts[hmobject_skill_id] != '')
+	    					$gameMessage.add(object_texts[hmobject_skill_id].replace('%1', $dataSkills[parseInt(hmobject_skill_id)].name));
+
+	    				if(typeof hm_prompts[hmobject_skill_id] === 'undefined' || hm_prompts[hmobject_skill_id]){
+		    				if($gameParty.members().some(function(actor) {return actor.hasSkill(hmobject_skill_id)})){
+		    					$gameMessage.add('Use '+$dataSkills[parseInt(hmobject_skill_id)].name+'?');
+		    					$gameMap._interpreter.setupChoices([['Yes', 'No'], 1]);
+		    				}
+		    				$gameMessage.setChoiceCallback(function(responseIndex) {
+							  	if (responseIndex === 0) {
+								    // Player chose "Yes"
+								    let map_id = $gameMap._mapId;
+									let key = [map_id, event_id, 'C'];
+									$gameSelfSwitches.setValue(key, true);
+									resetHmStatusArray();
+									hm_status_array[hmobject_skill_id] = true;
+							  	}
+							  	else {
+							    	// Player chose "No"
+							  	}
+							});
+	    				}
     				}
-    				$gameMessage.setChoiceCallback(function(responseIndex) {
-					  if (responseIndex === 0) {
-					    // Player chose "Yes"
-					    let map_id = $gameMap._mapId;
-						let key = [map_id, event_id, 'C'];
-						$gameSelfSwitches.setValue(key, true);
-						resetHmStatusArray();
-						hm_status_array[hmobject_skill_id] = true;
-					  }
-					  else {
-					    // Player chose "No"
-					  }
-					});
+    				else{
+    					console.log('Dpahoe_HiddenMoves: No skill with skill_id '+hmobject_skill_id+' is defined in Plugin parameters!');
+    				}
     			}
     		break;
 
@@ -219,7 +274,8 @@
 			if(typeof hm_status_array[skill.id] !== 'undefined'){
 				resetHmStatusArray();
 				hm_status_array[skill.id] = true;
-	    		$gameMessage.add(skill_texts[skill.id].replace('%1', $dataSkills[parseInt(skill.id)].name));
+				if(skill_texts[skill.id] != '')
+	    			$gameMessage.add(skill_texts[skill.id].replace('%1', $dataSkills[parseInt(skill.id)].name));
 	    	}
 		}
     }
